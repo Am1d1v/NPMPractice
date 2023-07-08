0b09d476d8d8de2aad2c044d2b33c5b28d668a4f
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-08 16:51:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 
 
 
-import { cloneDeep } from "lodash";
+import  cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+
+const state = {
+    cart: [
+        {product: "Banana", quantity: 10},
+        {product: "Apple", quantity: 5}
+    ],
+    user: {
+        loggedIn: true
+    }
+}
+
+const stateCopy = Object.assign({}, state);
+console.log(stateCopy);
